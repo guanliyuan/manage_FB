f478354532548cd790a7f9965f7815786543b713
YUnfeid <1173393116@qq.com>
2018-03-24 12:07:17 +0800

    增加跳转基础控制层skip

---
+++ src/main/java/com/FBmanage/ibs/controller/SkipController.java
@@ -0,0 +1,25 @@
+package com.FBmanage.ibs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+* @Author: lcc
+* @Description: 控制页面跳转的基础层
+* @Date: 12:05 2018/3/24
+* @params  * @param null
+*/
+@Controller
+@RequestMapping("/skip")
+public class SkipController {
+
+    /*   控制页面跳转基础层   */
+
+    /*  人事  */
+    /*  行政  */
+    /*  财务  */
+
+
+
+
+}
